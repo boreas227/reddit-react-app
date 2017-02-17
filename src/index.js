df4da73f857d821145e6f1b0ca0f5876a879fefb
Hodges and Coulter <duedter@gmail.com>
2017-02-17 09:56:40 -0600

    After adding thunk and Aync Calls

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
+import 'babel-polyfill';
+import thunkMiddleWare from 'redux-thunk';
+import createLogger from 'redux-logger';
+import {createStore, applyMiddleware} from 'redux';
+import {selectSubreddit, fetchPosts, fetchPostsIfNeeded} from './actions/actions';
+import rootReducer from './reducers/reducers';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+const loggerMiddleware = createLogger();
+
+const store = createStore(
+    rootReducer,
+    applyMiddleware(
+        thunkMiddleWare,
+        loggerMiddleware
+    )
 );
+
+store.dispatch(fetchPostsIfNeeded('reactjs')).then(() =>
+    console.log(store.getState())
+);
+
+// ReactDOM.render(
+//     <App />,
+//     document.getElementById('root')
+// );
